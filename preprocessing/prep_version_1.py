49560ee98fc57c13b64381ad720160d1af7f97a7
MHuberFaust <michael.huber@stud-mail.uni-wuerzburg.de>
2016-05-19 13:49:01 +0200

    funtkioniert für einzelne Pfade, die schleife darüber geht nicht

--- preprocessing/prep_version_1.py
+++ preprocessing/prep_version_1.py
@@ -5,6 +5,9 @@ folgende Ordnerstruktur sollte erstellt werden:
 Jahr -> Band ->Titel1
             ->Titel2
             ->Titel3
+            
+Wo geht die Reihenfolge verloren? (die Texte werden in der falschen Reihenfolge zusammengeklebt)
+-> liegt es an dem returnvalue von etree.xpath?
 
 '''
 
@@ -12,19 +15,27 @@ from lxml import etree
 from _collections import defaultdict
 import re
 import os.path
+import glob
+
 
 
 
 def parseXML(directory):
     '''
-    maybe add a parser?
+  
     
     
     '''
     mets = etree.parse(directory)
+    xpathVolume ='//mets:structMap//mets:div[@TYPE="volume"]/@LABEL'
+    filePathYear = mets.xpath(xpathVolume, namespaces={'mets':'http://www.loc.gov/METS/'})
+    
+    print(filePathYear)
+    #'/Users/MHuber/Documents/SS2016/TopicModelling/
     structLink= mets.xpath('/mets:mets/mets:structLink/mets:smLink', namespaces={'mets':'http://www.loc.gov/METS/',
                                                                                  'xlink':'http://www.w3.org/1999/xlink'})
     #print(structLink)
+    
     d = defaultdict(list)
     for child in structLink:
         k = child.get('{http://www.w3.org/1999/xlink}from')
@@ -41,24 +52,29 @@ def parseXML(directory):
    
         
     for item in d:
-        xpathE ='//mets:structMap[@TYPE="LOGICAL"]//mets:div[@ID="'+item+'"]'
+        xpathE ='//mets:structMap[@TYPE="LOGICAL"]//mets:div[@ID="'+item+'"]' #kennt das etree-objekt seine Eltern/Großeltern?
         structMap = mets.xpath(xpathE, namespaces= {'mets':'http://www.loc.gov/METS/'})
         #print(structMap)
         structMapElement = structMap[0]
         #print(etree.tostring(structMapElement, pretty_print=True))
-        print(structMapElement.get('LABEL'))
+        #print(structMapElement.get('LABEL'))
         if structMapElement.get('LABEL') != None:
-            label = structMapElement.get('LABEL')
+            xpathVolume ='//mets:structMap//mets:div[@TYPE="volume"]/@LABEL'
+            filePathYear = mets.xpath(xpathVolume, namespaces={'mets':'http://www.loc.gov/METS/'})
+            label = filePathYear[0]+'/'+ structMapElement.get('LABEL')
+            #print (label)
             fileList = d[item]
-            print (fileList)
+            #print (fileList)
             createFile(label, fileList)
+            #dir1 als parameter mitgeben um die Information in welchem Band der Artikel zu finden ist zu kreieren
 
 def createFile(label, fileList):
     '''
     dir1 = wo die Datein hinsollen
     dir2 = wo die Datein herkommen
     '''
-    dir1 = '/Users/MHuber/Documents/SS2016/TopicModelling/noXML/' + label
+    dir1 = '/Users/MHuber/Documents/SS2016/TopicModelling/newTry/' + label+'.txt'
+    #print(dir1)
     dir2 = '/Users/MHuber/Documents/SS2016/TopicModelling/1913-20/vls-suubdfggb-1913-341897/fulltext/'
     missingFiles=[]
     for item in fileList:
@@ -68,6 +84,8 @@ def createFile(label, fileList):
         if os.path.isfile(dir2 + item[4:]+'.xml'):
             with open (dir2 + item[4:]+'.xml','r', encoding = "utf-8") as r:
                 data = r.read()
+                if not os.path.exists(os.path.dirname(dir1)):
+                    os.makedirs(os.path.dirname(dir1))
                 with open (dir1, 'a', encoding = "utf-8" ) as f:
                     f.write(re.sub('¬ ', '', (re.sub('\n*<[^>]*>*\n*', '', (re.sub('</line>', ' ', data))))))
         else:
@@ -76,7 +94,12 @@ def createFile(label, fileList):
         
         
 
-parseXML('/Users/MHuber/Documents/SS2016/TopicModelling/1913-20/vls-suubdfggb-1913-341897/export_mets.xml')
+for item in glob.glob('/Users/MHuber/Documents/SS2016/TopicModelling/1913-20/*/export_mets.xml'):
+    print ('doing: ',item)
+    parseXML(item)
+    print(item, ': done')
+
+#parseXML('/Users/MHuber/Documents/SS2016/TopicModelling/1913-20/vls-suubdfggb-1913-341897/export_mets.xml')
         
     
     
